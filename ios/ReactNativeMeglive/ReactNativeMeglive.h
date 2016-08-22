766f0773759dc1f36cbf25d4f8afd2e2a3372ce7
Adi <adi@imeth.cn>
2016-08-22 17:07:20 +0800

    [添加] ios静态库项目

---
+++ ios/ReactNativeMeglive/ReactNativeMeglive.h
@@ -0,0 +1,13 @@
+//
+//  ReactNativeMeglive.h
+//  ReactNativeMeglive
+//
+//  Created by 戴文龙 on 16/8/22.
+//  Copyright © 2016年 戴文龙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactNativeMeglive : NSObject
+
+@end
---
+++ ios/ReactNativeMeglive/ReactNativeMeglive.m
@@ -0,0 +1,13 @@
+//
+//  ReactNativeMeglive.m
+//  ReactNativeMeglive
+//
+//  Created by 戴文龙 on 16/8/22.
+//  Copyright © 2016年 戴文龙. All rights reserved.
+//
+
+#import "ReactNativeMeglive.h"
+
+@implementation ReactNativeMeglive
+
+@end
